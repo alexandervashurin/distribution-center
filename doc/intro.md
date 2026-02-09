6876488e6b76c094ee1805d37d66efb748aaee5a
Вашурин Александр Юрьевич <a.vashurin@egov66.ru>
2026-02-09 16:36:46 +0500

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to distribution-center
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/distribution_center/core_test.clj
@@ -0,0 +1,7 @@
+(ns distribution-center.core-test
+  (:require [clojure.test :refer :all]
+            [distribution-center.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
